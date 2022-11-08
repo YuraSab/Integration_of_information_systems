bb8f65112c7b9c22a9cea0cf08d9a5d75ba5cfcb
Yura Sabat <yrasabat@gmail.com>
2022-11-08 18:50:15 +0200

    v2

---
+++ src/Routes/RoutesMassive.js
@@ -0,0 +1,10 @@
+export const publicRoutes = [
+
+];
+
+export const privateRoutes = [
+    {path: "/", name: "Home"},
+    {path: "estimate", name: "Estimate"},
+    {path: "contact", name: "Contacts"},
+    {path: "constructor", name: "Constructor"},
+];
--- src/elements/Header/Header.js
+++ src/elements/Header/Header.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import styles from "./Header.module.css";
+import {privateRoutes} from "../../Routes";
+import {Link} from "react-router-dom";
 
 const Header = () => {
+
+
     return (
         <div className={styles.main}>
-            Header
+            {
+                privateRoutes.map(value => <Link to={value.path} className={styles.link}>
+                    {value.name}
+                </Link>)
+            }
         </div>
     );
 };
